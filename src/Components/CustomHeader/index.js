91614c7c598b5115e91e512f73fa3ac1f2d183ee
TranVanThinh2802 <55169349+TranVanThinh2802@users.noreply.github.com>
2020-05-17 14:28:20 +0700

    #1 design home

--- src/Components/CustomHeader/index.js
+++ src/Components/CustomHeader/index.js
@@ -3,7 +3,7 @@ import style from './style.module.css'
 import { Drawer, Button } from 'rsuite';
 import { Switch, Route, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faFilm, faLaptop, faPlayCircle, faDove, faCamera, faStar, faVideo, faVideoSlash, faTv, faTablet  } from '@fortawesome/free-solid-svg-icons'
 
 
 function CustomHeader() {
@@ -15,34 +15,35 @@ function CustomHeader() {
     <React.Fragment>
       <header>
         <nav>
-       <FontAwesomeIcon icon={faCoffee} />
+
           {/* <Button onClick = {()=>setIsShow(!isShow)}> Clickffff me</Button> */}
           <div className={style.nav_left}>
-            
-              <div style={{ height: height - 100 }} className={style.title}>
-              <div className = {style.left}>
-                <h1>MOVIEBAZAR</h1>
-               <div className ={style.left_top}>
-                  <Link style={{ paddingTop: "30px" }} className={style.text_link} to='/Film'>Film</Link>
+            <div style={{ height: height - 100 }} className={style.title}>
+              <div className={style.left}>
+                <h1 style={{ paddingTop: 50 }}>DOO.NET</h1>
+                <div className={style.left_top}>
+                  <Link style={{ paddingTop: "30px" }} className={style.text_link} to='/Film'>Phim</Link>
                   <Link className={style.text_link} to='/Film'>Series</Link>
-                  <Link className={style.text_link} to='/Film'>My List</Link>
-               </div>
-              
-              
+                  <Link className={style.text_link} to='/My List'>Danh sách của tôi</Link>
+                </div>
+
+
                 <div className={style.left_center}>
-                  <Link className={style.text_link} to='/all-film'>All Flim</Link>
-                  <Link className={style.text_link} to='/Features'>Features</Link>
-                  <Link className={style.text_link} to='/Documents'>Documents</Link>
-                  <Link className={style.text_link} to='/Shorsy'>Shorst</Link>
-                  <Link className={style.text_link} to='/Show'>TV Shows</Link>
-                  <Link className={style.text_link} to='/About'>About</Link>
+                  <Link className={style.text_link} to='/all-film' ><FontAwesomeIcon className={style.icon_child_center} icon={faFilm}></FontAwesomeIcon> Tất cả phim</Link>
+                  <Link className={style.text_link} to='/Features'><FontAwesomeIcon className={style.icon_child_center} icon={faStar}></FontAwesomeIcon>Phim truyện</Link>
+                  <Link className={style.text_link} to='/Documents'><FontAwesomeIcon className={style.icon_child_center} icon={faVideo}></FontAwesomeIcon>Phim tài liệu</Link>
+                  <Link className={style.text_link} to='/Shorsy'><FontAwesomeIcon className={style.icon_child_center} icon={faVideoSlash}></FontAwesomeIcon>Phim ngắn</Link>
+                  <Link className={style.text_link} to='/Show'><FontAwesomeIcon className={style.icon_child_center} icon={faTv}></FontAwesomeIcon>Chương trình TV</Link>
+                  <Link className={style.text_link} to='/About'><FontAwesomeIcon className={style.icon_child_center} icon={faTablet}></FontAwesomeIcon>Về chúng tôi</Link>
                 </div>
                 <div className={style.icon}>
-                  <div style={{ height: "20px", width: "20px" }} />
+
+                  <FontAwesomeIcon className={style.icon_child} style = {{color: '386BBA'}} icon={faLaptop}></FontAwesomeIcon>
+
+                  <FontAwesomeIcon className={style.icon_child} style = {{color: 'E2BED8'}} icon={faPlayCircle}></FontAwesomeIcon>
+                  <FontAwesomeIcon className={style.icon_child} style = {{color: '439CE1'}} className={style.icon_child} icon={faDove}></FontAwesomeIcon>
+                  <FontAwesomeIcon className={style.icon_child} style = {{color: 'E090DA'}} icon={faCamera}></FontAwesomeIcon>
                 </div>
-              </div>
-              <div className={style.icon}>
-                <div style={{ height: "20px", width: "20px" }} />
               </div>
             </div>
 
@@ -54,4 +55,4 @@ function CustomHeader() {
   );
 }
 
-export default CustomHeader;
+export default CustomHeader;  
