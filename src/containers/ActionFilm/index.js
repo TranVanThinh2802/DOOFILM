339d8453f4c62fd76cce93cdc29e1862f3140a28
TranVanThinh2802 <55169349+TranVanThinh2802@users.noreply.github.com>
2020-08-10 16:12:42 +0700

    playFilm success

--- src/containers/ActionFilm/index.js
+++ src/containers/ActionFilm/index.js
@@ -2,7 +2,36 @@ import React, { useEffect, useState } from "react";
 import style from "./style.module.css";
 import CustomHeader from "../../Components/CustomHeader";
 import { Box } from "@material-ui/core";
+import {detailFilmAction} from '../../Redux/Action/detailFilmActions'
+import { Link, useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+
+
+
 function ActionFilm() {
+  const dispatch = useDispatch();
+  let {id} = useParams();
+  const [dataFilm, setDataFilm] = useState({});
+  
+
+  useEffect(()=>{
+    //dispatch gui payload la id  
+    dispatch(detailFilmAction(id, function(data){
+      // code ==200 chay function
+      // update lai DataFilm
+        setDataFilm(data);
+      }
+    ));
+  },[])
+  
+
+    useEffect(()=>{
+      console.log(dataFilm, "change")
+    },[dataFilm])
+
+    console.log(id)
+    
   return (
     <div>
       <div>
